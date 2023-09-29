e4b7d028dfcaf683c1247f082e0d8d513ab45e7c
Pablo Mariño Boga <pablo.marino.boga@gmail.com>
2023-09-29 07:26:01 +0200

    Adds bulk uploading documents

--- bulk_post_documents.py
+++ bulk_post_documents.py
@@ -1,4 +1,5 @@
 from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import RequestError
 import json
 
 json_file_path = 'data/output.json'
@@ -31,11 +32,27 @@ mapping = {
         }
     }
 }
-print("@@@@@@", es_client.info())
+
+if not es_client.indices.exists(index=index_name):
+    try:
+        # Crea el índice si no existe
+        es_client.indices.create(index=index_name)
+        print(f"El índice '{index_name}' se ha creado con éxito.")
+    except RequestError as e:
+        print(f"Error al crear el índice: {e}")
+else:
+    print(f"El índice '{index_name}' ya existe.")
+
+# print("@@@@@@", es_client.info())
 with open(json_file_path, "r", encoding="iso-8859-1") as json_file:
     # Load the JSON data into a Python dictionary or list
     data = json.load(json_file)
 
+
+
 for item in data:
-    resp = es_client.index(index=index_name, document=item)
-    print(resp)
+    try:
+        es_client.index(index=index_name, document=item)
+        print(f"Documento de la página {item['document_page']} agregado con éxito.")
+    except RequestError as e:
+        print(f"Error al agregar documento: {e}")
--- define_start_urls.py
+++ define_start_urls.py
@@ -1,5 +1,7 @@
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import json
+
+
 def define_start_urls():
     date = datetime.now()
     start_urls = []
@@ -8,21 +10,20 @@ def define_start_urls():
         month = date.strftime("%m")
         day = date.strftime("%d")
         date -= timedelta(days=1)
-        start_urls.append(f'https://www.xunta.gal/diario-oficial-galicia/mostrarContenido.do?ruta=/u01/app/oracle/shared/resources/pxdog100/doga/Publicados/{year}/{year}{month}{day}/Secciones1_gl.html')
+        start_urls.append(
+            f'https://www.xunta.gal/diario-oficial-galicia/mostrarContenido.do?ruta=/u01/app/oracle/shared/resources/pxdog100/doga/Publicados/{year}/{year}{month}{day}/Secciones1_gl.html')
     # print(start_urls)
     json_file_path = 'data/start_urls.json'
+    content = {"urls": start_urls}
     try:
         # Open the JSON file in write mode and save the data
         with open(json_file_path, 'w') as json_file:
-            json.dump(start_urls, json_file, indent=4)  # indent for pretty formatting (optional)
+            json.dump(content, json_file, indent=4)  # indent for pretty formatting (optional)
 
         print(f"Data saved to '{json_file_path}' successfully.")
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-
-
-
 
 
 define_start_urls()
